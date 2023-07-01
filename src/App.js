a2ec30d50cd8fe237a3a56a26febf49fa08b1775
Tony <thunter1987@yahoo.com>
2023-07-01 17:25:14 -0500

    start fetches

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([])
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((res) => res.json())
+      .then((data) => setPokemonList(data));
+  });
   return (
-    <div className="App">
-      <header>
-        <p>
-          {/* insert code here */}
-       </p>
-      </header>
+    <div className='App'>
+      { pokemonList.map(pokemon => <li> pokemon </li>) }
     </div>
   );
 }
