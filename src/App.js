c3c9334c7a3fba6ec9e8018ff8a9ae4b6c4607a9
Tony <thunter1987@yahoo.com>
2023-07-02 00:38:08 -0500

    adds API fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
-  const [pokemonList, setPokemonList] = useState([])
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-      .then((res) => res.json())
-      .then((data) => setPokemonList(data));
-  });
+  const [pokemonName, setPokemonName] = useState("");
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+        console.log(response)
+      }
+    );
+  };
   return (
     <div className='App'>
-      { pokemonList.map(pokemon => <li> pokemon </li>) }
+      <h1>Pokemon Info</h1>
+      <input
+        className="pokemon-search-input"
+        type='text'
+        onChange={(event) => setPokemonName(event.target.value)}/>
+      <button onClick={searchPokemon}>Search Pokemon</button>
     </div>
   );
 }
