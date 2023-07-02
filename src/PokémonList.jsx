588024a224f5a44d6199590440d653c09c9697f8
Tony <thunter1987@yahoo.com>
2023-07-02 13:23:11 -0500

    shows name list on component

--- src/PokémonList.jsx
+++ src/PokémonList.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function PokémonList() {
-
+    const [pokémonData, setPokémonData] = useState([]);
+    
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
-            console.log(response.data.results);
+            setPokémonData(response.data.results);
         }
         fetchData();
     }, []);
   return (
-    <div>PokémonList</div>
-  )
-}
+      <div>{ pokémonData.map(pokemon => (
+          <li key={ pokemon.name }>{ pokemon.name }</li>
+    ))}</div>
+  )}
 
 export default PokémonList
