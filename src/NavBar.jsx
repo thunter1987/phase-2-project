7d465c84923327a75ed4a583fa9c7cff11543566
Tony <thunter1987@yahoo.com>
2023-07-03 15:54:58 -0500

    more NavBar functionality

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className='nav'>Performance Parts Manager</nav>
-  )
+    <nav className='nav'>
+      <Link to='/' className='site-title'>
+        Performance Parts Manager
+          </Link>
+          <ul>
+              <CustomLink to='/parts'>Parts List</CustomLink>
+              <CustomLink to='/partform'>Parts Form</CustomLink>              
+          </ul>
+    </nav>
+  );
+    function CustomLink({to, children, ...props}) {
+        const path = window.location.pathname
+        return (
+            <li className={ path === to ? "active" : "" }>
+                <Link to={ path } { ...props }>{ children }</Link>
+            </li>
+        )
+    }
 }
-
