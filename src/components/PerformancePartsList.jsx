8cffc2952992a42041035fd987fa9c5153997bf1
thunter1987 <116783221+thunter1987@users.noreply.github.com>
2023-07-10 05:08:14 -0500

    Update PerformancePartsList.jsx

--- src/components/PerformancePartsList.jsx
+++ src/components/PerformancePartsList.jsx
@@ -5,12 +5,13 @@ import { url } from "../constants";
 function PerformancePartsList() {
   const [performanceParts, setPerformanceParts] = useState([]);
       
-  useEffect(async function getPartsList () {
+  useEffect(async function getPartsList() {
     const partsList = await fetch(url.parts)
     .then((r) => r.json())
       setPerformanceParts(partsList)
       }
-  }getPartsList, []);
+      getPartsList()
+  }, []);
 
   const handleDelete = (id) => {
     const config = { method: "DELETE" };
